7c206857640dc83ea5d160a45cf1b36ddcec5a70
SydNichols <sydneynichols.m@gmail.com>
2025-09-24 13:35:01 -0400

    fixed api call
    included local host address in API call

--- client/components/Courses.jsx
+++ client/components/Courses.jsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchCourseList = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/courses');
+                if(!response.ok) {
+                    throw new Error(`HTTP ${response.status}`);
+                }
+                const courses = await response.json();
+                setCourses(courses);
+            } catch (err) {
+                setError(err.message);
+            }
+        }
+
+        fetchCourseList();
+    }, []);
+    
+    console.log(courses)
+
     return (
-        <h1>Hello World</h1>
+        <main>
+            <div className="wrap main--grid">
+                {courses.map(course => (
+                    <>
+                        <h2 className="course--label">Course</h2>
+                        <h3 className="course--title">{course.title}</h3>
+                    </>
+                ))}
+            </div>
+        </main>
     );
 };
 
